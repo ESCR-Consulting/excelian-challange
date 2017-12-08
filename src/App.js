a91177b3ec314c2173a752aa0227fa76a65abd85
Sam Latif <escr.consulting@gmail.com>
2017-12-08 09:51:02 +0000

    Add redux state container

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { MuiThemeProvider } from 'material-ui/styles';
 import theme from './MUITheme';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import ResponsiveDrawer from './ResponsiveDrawer';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'TOGGLE_DRAWER':
+            return {
+                showDrawer: !state.showDrawer
+            }
+        default:
+            return state
+    }
+}
+
+const store = createStore(reducer)
 
 class App extends React.Component {
     render() {
         return (
-            <MuiThemeProvider theme={theme}>
-                <ResponsiveDrawer />
-            </MuiThemeProvider>
+            <Provider store={store}>
+                <MuiThemeProvider theme={theme}>
+                    <ResponsiveDrawer />
+                </MuiThemeProvider>
+            </Provider>
         );
     }
 }
