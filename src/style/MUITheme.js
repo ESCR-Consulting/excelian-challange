75f1acde19dbe5d18a4ec10c1b8b06a7a4bddb83
Sam Latif <escr.consulting@gmail.com>
2017-12-09 14:22:08 +0000

    Use correct colour pallete

--- src/style/MUITheme.js
+++ src/style/MUITheme.js
@@ -1,12 +1,12 @@
 import { createMuiTheme } from 'material-ui/styles';
-import indigo from 'material-ui/colors/indigo';
-import green from 'material-ui/colors/green';
+import blue from 'material-ui/colors/blue';
+import orange from 'material-ui/colors/orange';
 import red from 'material-ui/colors/red';
 
 const theme = createMuiTheme({
     palette: {
-        primary: indigo,
-        secondary: green,
+        primary: blue,
+        secondary: orange,
         error: red,
     },
 });
